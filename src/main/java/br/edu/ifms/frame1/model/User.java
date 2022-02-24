b1f1b3db03b5917679155d0d1f9344aa5806d1e9
Mauricio-Silva <sir.silvabmauricio@gmail.com>
2022-02-23 22:48:35 -0400

    Aula_23_02_2022_dbf_I

--- src/main/java/br/edu/ifms/frame1/model/User.java
+++ src/main/java/br/edu/ifms/frame1/model/User.java
@@ -2,6 +2,13 @@ package br.edu.ifms.frame1.model;
 
 import java.util.UUID;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,8 +16,13 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     UUID id;
+    @Column(name = "userName", nullable = false)
+    @NotBlank
     String nome;
     String email;
 }
@@ -18,3 +30,8 @@ public class User {
 //Data - Getters e Setters
 //AllArgsConstructor - Construtor Completo Automático
 //NoArgsConstructor - Retira Construtor de Default
+//Entity - Transforma a Classe em Tabela
+//Id - Define o Atributo da Classe como sendo o Atributo Idemtificador(ID) da Tabela
+//GeneratedValue - Gera Valores para o Atributo
+//Column - Define Especifições da Coluna
+//NotBlank - Não Nulo
--- src/main/java/br/edu/ifms/frame1/repository/UserRepository.java
+++ src/main/java/br/edu/ifms/frame1/repository/UserRepository.java
@@ -1,29 +1,13 @@
 package br.edu.ifms.frame1.repository;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.edu.ifms.frame1.model.User;
 
 @Repository
-public class UserRepository {
+public interface UserRepository extends JpaRepository<User, UUID>{
 
-    private static final Logger logger = LoggerFactory.getLogger(UserRepository.class);
-
-    //SELECT * FROM TB_USER
-    public List<User> findAll() {
-        var users = new ArrayList<User>();
-        users.add(new User(UUID.randomUUID(), "Mauricio", "sir.Mau@gmail.com"));
-        users.add(new User(UUID.randomUUID(), "Galadriel", "sir.Gal@gmail.com"));
-        users.add(new User(UUID.randomUUID(), "Elrond", "sir.Elr@gmail.com"));
-        users.add(new User(UUID.randomUUID(), "Gil-Galad", "sir.Gil@gmail.com"));
-        logger.info("Listing the Users!");
-
-        return users;
-    }   
 }
